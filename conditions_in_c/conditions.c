4597a73e9e0cc73315427bcb1f9e3cea95fc2872
ABDELMALEK ACHKIF <malikhkif@gmail.com>
2022-09-12 11:31:03 +0100

    X

--- conditions_in_c/conditions.c
+++ conditions_in_c/conditions.c
@@ -3,14 +3,14 @@
 
 /* 
 
- - Vous pouvez utiliser ces conditions pour effectuer différentes actions pour différentes décisions.
+ - Vous pouvez utiliser ces conditions pour effectuer diffï¿½rentes actions pour diffï¿½rentes dï¿½cisions.
 
- - C contient les instructions conditionnelles suivantes :
+ - C contient les instructions conditionnelles suivantesï¿½:
 
-	Utilisez if pour spécifier un bloc de code à exécuter, si une condition spécifiée est vraie
-	Utilisez else pour spécifier un bloc de code à exécuter, si la même condition est fausse
-	Utilisez else if pour spécifier une nouvelle condition à tester, si la première condition est fausse
-	Utilisez switch pour spécifier de nombreux blocs de code alternatifs à exécuter 
+	Utilisez if pour specifier un bloc de code ï¿½ exï¿½cuter, si une condition spï¿½cifiï¿½e est vraie
+	Utilisez else pour specifier un bloc de code ï¿½ exï¿½cuter, si la mï¿½me condition est fausse
+	Utilisez else if pour spï¿½cifier une nouvelle condition ï¿½ tester, si la premiï¿½re condition est fausse
+	Utilisez switch pour spï¿½cifier de nombreux blocs de code alternatifs ï¿½ exï¿½cuter 
 	
 */
 
@@ -19,11 +19,11 @@ int main(){
 	
 //    =====   Les Conditions   =====
 	// L'instruction if  : 
-	// L'instruction if est la structure de test la plus basique, on la retrouve dans tous les langages (avec une syntaxe différente...). Elle permet 
-    // d'exécuter une série d'instructions si jamais une condition est réalisée.
+	// L'instruction if est la structure de test la plus basique, on la retrouve dans tous les langages (avec une syntaxe diffï¿½rente...). Elle permet 
+    // d'exï¿½cuter une sï¿½rie d'instructions si jamais une condition est rï¿½alisï¿½e.
 	
 	// La syntaxe de cette expression est la suivante   :
-		// if (condition réalisée) {
+		// if (condition rï¿½alisï¿½e) {
 		//    liste d'instructions;
 		// }
 		
@@ -34,11 +34,11 @@ int main(){
 		
 		
 	// Remarques   :
-		// la condition doit être entre des parenthèses, il est possible de définir plusieurs conditions à remplir avec les opérateurs ET et OU (&& et ||)
+		// la condition doit ï¿½tre entre des parenthï¿½ses, il est possible de dï¿½finir plusieurs conditions ï¿½ remplir avec les opï¿½rateurs ET et OU (&& et ||)
 		// Par exemple l'instruction suivante teste si les deux conditions sont vraies :  if ((condition1)&&(condition2)) .
-		// L'instruction suivante exécutera les instructions si l'une ou l'autre des deux conditions est vraie :   if ((condition1)||(condition2))  .
-		// s'il n'y a qu'une instruction, les accolades ne sont pas nécessaires...
-		// les instructions situées dans le bloc qui suit else sont les instructions qui seront exécutées si la ou les conditions ne sont pas remplies
+		// L'instruction suivante exï¿½cutera les instructions si l'une ou l'autre des deux conditions est vraie :   if ((condition1)||(condition2))  .
+		// s'il n'y a qu'une instruction, les accolades ne sont pas nï¿½cessaires...
+		// les instructions situï¿½es dans le bloc qui suit else sont les instructions qui seront exï¿½cutï¿½es si la ou les conditions ne sont pas remplies
 		
 	// Exemple  : 
 		if (18 > 20) printf("20 is greater than 18\n");  
@@ -46,29 +46,29 @@ int main(){
 
 	//L'instruction if ... else   :
 		// 'instruction if dans sa forme basique ne permet de tester qu'une condition, or la plupart du temps on aimerait pouvoir choisir les instructions 
-		// à exécuter en cas de non réalisation de la condition...
-		// L'expression if ... else permet d'exécuter une autre série d'instructions en cas de non-réalisation de la condition.
+		// ï¿½ exï¿½cuter en cas de non rï¿½alisation de la condition...
+		// L'expression if ... else permet d'exï¿½cuter une autre sï¿½rie d'instructions en cas de non-rï¿½alisation de la condition.
 	
 		//La syntaxe de cette expression est la suivante :
 			
-			// if (condition réalisée) {
+			// if (condition rï¿½alisï¿½e) {
 			//   liste d'instructions
 			// }
 			//
 			// else {
-			// 	 autre série d'instructions
+			// 	 autre sï¿½rie d'instructions
 			// }
 			
-		// Une façon plus courte de faire un test  : 
-			// Il est possible de faire un test avec une structure beaucoup moins lourde grâce à la structure suivante :
+		// Une faï¿½on plus courte de faire un test  : 
+			// Il est possible de faire un test avec une structure beaucoup moins lourde grï¿½ce ï¿½ la structure suivante :
 			// (condition) ? instruction si vrai : instruction si faux
 			
 		// Remarques :
 		
-			// la condition doit être entre des parenthèses
-			// Lorsque la condition est vraie, l'instruction de gauche est exécutée
-			// Lorsque la condition est fausse, l'instruction de droite est exécutée
-			// En plus d'être exécutée, la structure ?: renvoie la valeur résultant de l'instruction exécutée. Ainsi, cette forme ?: est souvent utilisée comme suit :
+			// la condition doit ï¿½tre entre des parenthï¿½ses
+			// Lorsque la condition est vraie, l'instruction de gauche est exï¿½cutï¿½e
+			// Lorsque la condition est fausse, l'instruction de droite est exï¿½cutï¿½e
+			// En plus d'ï¿½tre exï¿½cutï¿½e, la structure ?: renvoie la valeur rï¿½sultant de l'instruction exï¿½cutï¿½e. Ainsi, cette forme ?: est souvent utilisï¿½e comme suit :
 			// position = ((enAvant == 1) ? compteur+1 : compteur-1);
 			 
 			// Exemple  : 
@@ -91,8 +91,8 @@ int main(){
 		    
 	// L'instruction switch    :
 	
-		// L'instruction switch permet de faire plusieurs tests de valeurs sur le contenu d'une même variable. Ce branchement conditionnel simplifie
-	    // beaucoup le test de plusieurs valeurs d'une variable, car cette opération aurait été compliquée (mais possible) avec des if imbriqués.
+		// L'instruction switch permet de faire plusieurs tests de valeurs sur le contenu d'une mï¿½me variable. Ce branchement conditionnel simplifie
+	    // beaucoup le test de plusieurs valeurs d'une variable, car cette opï¿½ration aurait ï¿½tï¿½ compliquï¿½e (mais possible) avec des if imbriquï¿½s.
 		// Sa syntaxe est la suivante :
 		
 		// switch (Variable) {
@@ -113,25 +113,52 @@ int main(){
 		// 	Liste d'instructions;
 		//}
 		
-		// Les parenthèses qui suivent le mot clé switch indiquent une expression dont la valeur est testée successivement par chacun des case.
-	    // Lorsque l'expression testée est égale à une des valeurs suivant un case, la liste d'instructions qui suit celui-ci est exécutée. 
-		// Le mot clé break indique la sortie de la structure conditionnelle. Le mot clé default précède la liste d'instructions qui sera exécutée si
-		// l'expression n'est jamais égale à une des valeurs.
+		// Les parenthï¿½ses qui suivent le mot clï¿½ switch indiquent une expression dont la valeur est testï¿½e successivement par chacun des case.
+	    // Lorsque l'expression testï¿½e est ï¿½gale ï¿½ une des valeurs suivant un case, la liste d'instructions qui suit celui-ci est exï¿½cutï¿½e. 
+		// Le mot clï¿½ break indique la sortie de la structure conditionnelle. Le mot clï¿½ default prï¿½cï¿½de la liste d'instructions qui sera exï¿½cutï¿½e si
+		// l'expression n'est jamais ï¿½gale ï¿½ une des valeurs.
 		
 		// Switch Example   :
+	       		 int day = 1;
+
+			switch (day) {
+			  case 1:
+			    printf("Monday");
+			    break;
+			  case 2:
+			    printf("Tuesday");
+			    break;
+			  case 3:
+			    printf("Wednesday");
+			    break;
+			  case 4:
+			    printf("Thursday");
+			    break;
+			  case 5:
+			    printf("Friday");
+			    break;
+			  case 6:
+			    printf("Saturday");
+			    break;
+			  case 7:
+			    printf("Sunday");
+			    break;
+			}
+
+			// Outputs "Monday" (day 1)
 	
 	//	Les boucles
-		//Les boucles sont des structures qui permettent d'exécuter plusieurs fois la même série d'instructions jusqu'à ce qu'une condition 
-		// ne soit plus réalisée...
-		//On appelle parfois ces structures instructions répétitives ou bien itérations.
-		//La façon la plus commune de faire une boucle est de créer un compteur (une variable qui s'incrémente, c'est-à-dire qui augmente de 1 à
-		// chaque tour de boucle) et de faire arrêter la boucle lorsque le compteur dépasse une certaine valeur.
+		//Les boucles sont des structures qui permettent d'exï¿½cuter plusieurs fois la mï¿½me sï¿½rie d'instructions jusqu'ï¿½ ce qu'une condition 
+		// ne soit plus rï¿½alisï¿½e...
+		//On appelle parfois ces structures instructions rï¿½pï¿½titives ou bien itï¿½rations.
+		//La faï¿½on la plus commune de faire une boucle est de crï¿½er un compteur (une variable qui s'incrï¿½mente, c'est-ï¿½-dire qui augmente de 1 ï¿½
+		// chaque tour de boucle) et de faire arrï¿½ter la boucle lorsque le compteur dï¿½passe une certaine valeur.
 		
 		// La boucle for
-			//L'instruction for permet d'exécuter plusieurs fois la même série d'instructions : c'est une boucle !
-			//Dans sa syntaxe, il suffit de préciser le nom de la variable qui sert de compteur (et éventuellement sa valeur de départ,
-			// la condition sur la variable pour laquelle la boucle s'arrête (basiquement une condition qui teste si la valeur du compteur dépasse une limite)
-			// et enfin une instruction qui incrémente (ou décrémente) le compteur.
+			//L'instruction for permet d'exï¿½cuter plusieurs fois la mï¿½me sï¿½rie d'instructions : c'est une boucle !
+			//Dans sa syntaxe, il suffit de prï¿½ciser le nom de la variable qui sert de compteur (et ï¿½ventuellement sa valeur de dï¿½part,
+			// la condition sur la variable pour laquelle la boucle s'arrï¿½te (basiquement une condition qui teste si la valeur du compteur dï¿½passe une limite)
+			// et enfin une instruction qui incrï¿½mente (ou dï¿½crï¿½mente) le compteur.
 			
 			//La syntaxe de cette expression est la suivante   :
 			
@@ -141,23 +168,51 @@ int main(){
 			//}
 			//Par exemple :
 			//
-			//for (i=1; i<6; i++) {
-			//	printf("%d", i);
-			//
-			//}
-			//Cette boucle affiche 5 fois la valeur de i, c'est-à-dire 1, 2, 3, 4, 5.
-			//Elle commence à i=1, vérifie que i est bien inférieur à 6, etc. jusqu'à atteindre la valeur i=6, pour laquelle la condition ne sera plus réalisée, la boucle s'interrompra et le programme continuera son cours.
+			for (int i=1; i<6; i++) {
+				printf("%d", i);
+		
+			}
+			//Cette boucle affiche 5 fois la valeur de i, c'est-ï¿½-dire 1, 2, 3, 4, 5.
+			//Elle commence ï¿½ i=1, vï¿½rifie que i est bien infï¿½rieur ï¿½ 6, etc. jusqu'ï¿½ atteindre la valeur i=6, pour laquelle la condition ne sera plus rï¿½alisï¿½e, la boucle s'interrompra et le programme continuera son cours.
 			//
 			//	
-			//il faudra toujours vérifier que la boucle a bien une condition de sortie (i.e. le compteur s'incrémente correctement)
-			//une instruction printf(); dans votre boucle est un bon moyen pour vérifier la valeur du compteur pas à pas en l'affichant !
-			//il faut bien compter le nombre de fois que l'on veut faire exécuter la boucle :
-			//for(i=0;i<10;i++) exécute 10 fois la boucle (i de 0 à 9)
-			//for(i=0;i<=10;i++) exécute 11 fois la boucle (i de 0 à 10)
-			//for(i=1;i<10;i++) exécute 9 fois la boucle (i de 1 à 9)
-			//for(i=1;i<=10;i++) exécute 10 fois la boucle (i de 1 à 10)	
-				
-		//		
+			//il faudra toujours vï¿½rifier que la boucle a bien une condition de sortie (i.e. le compteur s'incrï¿½mente correctement)
+			//une instruction printf(); dans votre boucle est un bon moyen pour vï¿½rifier la valeur du compteur pas ï¿½ pas en l'affichant !
+			//il faut bien compter le nombre de fois que l'on veut faire exï¿½cuter la boucle :
+			//for(i=0;i<10;i++) exï¿½cute 10 fois la boucle (i de 0 ï¿½ 9)
+			//for(i=0;i<=10;i++) exï¿½cute 11 fois la boucle (i de 0 ï¿½ 10)
+			//for(i=1;i<10;i++) exï¿½cute 9 fois la boucle (i de 1 ï¿½ 9)
+			//for(i=1;i<=10;i++) exï¿½cute 10 fois la boucle (i de 1 ï¿½ 10)	
+	// L'instruction while
+    	// L'instruction while reprÃ©sente un autre moyen d'exÃ©cuter plusieurs fois la mÃªme sÃ©rie d'instructions.
+
+		// La syntaxe de cette expression est la suivante :
+			// while (condition rÃ©alisÃ©e) {
+			// 	liste d'instructions;
+
+			// }
+        // Cette instruction exÃ©cute la liste d'instructions tant que (while est un mot anglais qui signifie tant que)
+		//  la condition est rÃ©alisÃ©e.
+
+		// La condition de sortie pouvant Ãªtre n'importe quelle structure conditionnelle, les risques de boucle infinie
+		//  (boucle dont la condition est toujours vraie) sont grands, c'est-Ã -dire qu'elle risque de provoquer un plantage
+		//  du programme en cours d'exÃ©cution !
+		// Exapmle : 
+			int x=1;
+
+			while (x<=10) {
+				if (x == 7) {
+					printf("Division par zÃ©ro !");
+
+					continue;
+				}
+				int a = 1/(x-7);
+
+				printf("%f", a);
+
+				x++;
+
+			}
 		    
 				 
 	
